c4ffe13942b23e63b5678755a11a80e7e1582732
eliezer <eliezer.c.alves2015@gmail.com>
2021-10-18 20:48:02 -0300

    fix(controllers): excluindo controlador inutil

--- app/Controllers/AuthController.py
+++
@@ -1,7 +0,0 @@
-from . import Controller
-
-class AuthController():
-
-    def login(self):
-        callback_url = self._request.args.get('callback_url') or ""
-        return self._render_template("login.html", callback_url=callback_url)
